4487ccebc7167a249e6e6a7cccb027df830078ff
luka chikashvili <lukachikashvili2020@gmail.com>
2025-04-28 02:11:59 +0400

    add parties to db

---
+++ client/src/app/(admin)/admin/parties/create/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+export const metadata = {
+    title: "პარტიის დამატება | fakeElection",
+    description: "დაამატე ახალი პარტია",
+  };
+
+
+
+
+export default function AddPartyPage() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-6">დაამატე პარტია</h1>
+            
+            
+        </div>
+    )
+}
---
+++ client/src/app/(admin)/admin/parties/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      parties
+    </div>
+  )
+}
+
+export default page
